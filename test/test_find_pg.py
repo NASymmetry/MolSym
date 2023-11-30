9fc6804ca0c96d8ccd90fee5900be9c5d3f3010b
Andreas Copan <avcopan@gmail.com>
2023-11-30 10:29:27 -0500

    Allows tests to be run from other locations

--- test/test_find_pg.py
+++ test/test_find_pg.py
@@ -1,8 +1,11 @@
+import os
 from molsym.flowchart import find_point_group
 #import psi4
 import qcelemental as qcel
 from molsym.molecule import Molecule
 import pytest
+
+PATH = os.path.dirname(os.path.realpath(__file__))
 
 names = ["C1", "Ci", "Cs", "Cs_nonplanar", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2", "D3", "D2d", "D4d", "D3h", "D4h", "D5h", "D6h", "D12h", "D100h", 
          "Cinfv", "Dinfh", "Th", "Td", "cube", "octahedron", "dodecahedron", "icosahedron"]
@@ -14,7 +17,8 @@ pgs = ["C1", "Ci", "Cs", "Cs", "C2", "C3", "S4", "C2v", "C3v", "C2h", "C3h", "D2
 
 @pytest.mark.parametrize("name, pg_ans", [(names[i], pgs[i]) for i in range(len(names))])
 def test_find_point_group(name, pg_ans):
-    with open("test/sxyz/"+name+".xyz", "r") as fn:
+    file_path = os.path.join(PATH, "sxyz", f"{name}.xyz")
+    with open(file_path, "r") as fn:
         strang = fn.read()
     schema = qcel.models.Molecule.from_data(strang).dict()
     mol = Molecule.from_schema(schema)
