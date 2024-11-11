Changing Ih sign again? Fixing formaldehyde saxis bug

molsym/pgdetect/flowchart.py
@@ -3,6 +3,19 @@ from ..symtools import *
 from .flowchart_helper import *
 
 def find_point_group(mol):
+    """
+    Find the point group of a molecule.
+    Bases on the algorithm developed by:
+        Beruski, Otávio; Vidal, Luciano N. Algorithms for computer detection of 
+        symmetry elements in molecular systems, J. Comp. Chem, 2013 doi:10.1002/jcc.23493
+    Returns a primary and secondary axis in order to define an orienation of the molecule
+    with resepct to the symmetry elements generated later.
+    
+    :type mol: molsym.Molecule
+    :return: Schoenflies point group string, primary axis, and secondary axis
+    :rtype: (str, NumPy array of shape (3,), NumPy array of shape (3,))
+    """
+
     paxis = [0,0,0]
     saxis = [0,0,0]
     moit = calcmoit(mol)
@@ -32,7 +45,7 @@ def find_point_group(mol):
             # Reorienting vectors such that one face is on the z-axis with "pentagon" pointing at the POSITIVE y-axis
             phi = (1+np.sqrt(5.0))/2
             # Weirdness here, negative or positive???
-            theta =-np.arccos(phi/np.sqrt(1+(phi**2)))
+            theta = np.arccos(phi/np.sqrt(1+(phi**2)))
             rmat = rotation_matrix(saxis, theta)
             paxis = np.dot(rmat, tempaxis)
             taxis = np.dot(rmat, paxis)

molsym/symtools.py
@@ -2,6 +2,16 @@ import numpy as np
 from molsym.molecule import *
 
 def rotation_matrix(axis, theta):
+    """
+    Rotation matrix about an axis by theta in radians.
+
+    :param axis: Cartesian vector defining rotation axis
+    :param theta: Angle of rotation in radians
+    :type axis: NumPy array of shape (3,)
+    :type theta: float
+    :return: Matrix defining rotation on column vector
+    :rtype: NumPy array of shape (3,3)
+    """
     cos_t = np.cos(theta)
     sin_t = np.sin(theta)
     # NOT NORMALIZING AXIS!!!
@@ -11,6 +21,14 @@ def rotation_matrix(axis, theta):
     return M
 
 def reflection_matrix(axis):
+    """
+    Reflection matrix about a plane defined by its normal vector.
+
+    :param axis: Cartesian vector defining the plane normal vector
+    :type axis: NumPy array of shape (3,)
+    :return: Matrix defining reflection on column vector
+    :rtype: NumPy array of shape (3,3)
+    """
     M = np.zeros((3,3))
     for i in range(3):
         for j in range(i,3):
@@ -22,33 +40,82 @@ def reflection_matrix(axis):
     return M
 
 def inversion_matrix():
+    """
+    Cartesian inversion matrix.
+
+    :return: Matrix defining inversion
+    :rtype: NumPy array of shape(3,3)
+    """
     return -1*np.eye(3)
 
 def Cn(axis, n):
+    """
+    Wrapper around rotation_matrix for producing a C_n rotation about axis.
+
+    :param axis: Cartesian vector defining rotation axis
+    :param n: Defines rotation angle by theta = 2 pi / n
+    :type axis: NumPy array of shape (3,)
+    :type n: int
+    :return: Matrix defining proper rotation on column vector
+    :rtype: NumPy array of shape (3,3)
+    """
     theta = 2*np.pi/n
     return rotation_matrix(axis, theta)
 
 def Sn(axis, n):
+    """
+    Improper rotation S_n about an axis.
+    
+    :param axis: Cartesian vector defining rotation axis
+    :param n: Defines rotation angle by theta = 2 pi / n
+    :type axis: NumPy array of shape (3,)
+    :type n: int
+    :return: Matrix defining improper rotation on column vector
+    :rtype: NumPy array of shape (3,3)
+    """
     return np.dot(reflection_matrix(axis), Cn(axis, n))
 
 def isequivalent(A,B):
+    """
+    Returns True if molecule A and B are equivalent with respect to permutation of like atoms.
+
+    :param A: Molecule A
+    :param B: Molecule B
+    :type A: molsym.Molecule
+    :type B: molsym.Molecule
+    :return: True if equivalent, False if not
+    :rtype: bool
+    """
     if A.tol >= B.tol:
         eq_tol = A.tol
     else:
         eq_tol = B.tol
-    h = []
+    matched_already = []
     for i in range(A.natoms):
         for j in range(B.natoms):
-            if A.masses[i] == B.masses[j]:
-                zs = abs(A.coords[i,:]-B.coords[j,:])
-                if np.allclose(zs, [0,0,0], atol=eq_tol):
-                    h.append(j)
-                    break
-    if len(h) == A.natoms:
+            # Reduce search list so large molecules are a bit faster
+            if j not in matched_already:
+                # Check that masses are equal
+                if A.masses[i] == B.masses[j]:
+                    # Check if atoms are about at the same Cartesian point
+                    zs = abs(A.coords[i,:]-B.coords[j,:])
+                    if np.allclose(zs, [0,0,0], atol=eq_tol):
+                        matched_already.append(j)
+                        break
+    # Did we find a match for each atom? If so we win
+    if len(matched_already) == A.natoms:
         return True
     return False
 
 def calcmoit(atoms):
+    """
+    Calculates the moment of inertia tensor for a list of atoms.
+    
+    :param atoms: Set of atoms
+    :type atoms: molsym.Molecule
+    :return: Cartesian moment of inertia tensor
+    :rtype: NumPy array of shape (3,3)
+    """
     I = np.zeros((3,3))
     atoms.translate(atoms.find_com())
     for i in range(3):
@@ -62,12 +129,32 @@ def calcmoit(atoms):
     return I
 
 def normalize(a):
+    """
+    Normalize vector a to unit length, return None if the input vector is of zero length.
+
+    :param a: Vector of arbitrary magnitude
+    :type a: NumPy array of shape (n,)
+    :return: Normalized vector or None if the magnitude of a is less than the global tolerance
+    :rtype: NumPy array of shape (n,) or None
+    """
     n = np.linalg.norm(a)
     if n <= global_tol:
         return None
     return a / np.linalg.norm(a)
 
-def issame_axis(a, b, tol = global_tol):
+def issame_axis(a, b, tol=global_tol):
+    """
+    Return True if vectors a and b are colinear within the global tolerance.
+
+    :param a: Vector a
+    :param b: Vector b
+    :param tol: Tolerance for error
+    :type a: NumPy array of shape (n,)
+    :type b: NumPy array of shape (n,)
+    :type tol: float
+    :return: True if vectors are colinear, False if not colinear or if either vector has zero length
+    :rtype: bool
+    """
     A = normalize(a)
     B = normalize(b)
     if A is None or B is None:
@@ -76,17 +163,39 @@ def issame_axis(a, b, tol = global_tol):
     return np.isclose(d, 1.0, atol=tol)
 
 def isfactor(n,a):
+    """
+    Return True if a divides n.
+    
+    :type n: int
+    :type a: int
+    :rtype: bool
+    """
     if n % a == 0:
         return True
     else:
         return False
 
 def reduce(n, i):
+    """
+    Divide n and i by their greatest common divisor g.
+
+    :type n: int
+    :type i: int
+    :return: Tuple of n/g and i/g
+    :rtype: (int, int)
+    """
     g = gcd(n, i)
     return n//g, i//g # floor divide to get an int, there should never be a remainder since we are dividing by the gcd
 
 def gcd(A, B):
-    # A quick implementation of the Euclid algorithm for finding the greatest common divisor
+    """
+    A quick implementation of the Euclid algorithm for finding the greatest common divisor between A and B.
+    
+    :type A: int
+    :type B: int
+    :return: Greatest common divisor between A and B
+    :rtype: int
+    """
     a = max(A,B)
     b = min(A,B)
     if a == 0:
@@ -98,7 +207,14 @@ def gcd(A, B):
         return gcd(b, r)
 
 def divisors(n):
-    # This isn't meant to handle large numbers, thankfully most point groups have an order less than 100
+    """
+    Returns the divisors of n.
+    This isn't meant to handle large numbers, thankfully most point groups have an order less than 100
+    
+    :type n: int
+    :return: List of n's divisors
+    :rtype: List[int]
+    """
     out = []
     for i in range(n):
         if n % (i+1) == 0:
@@ -106,4 +222,12 @@ def divisors(n):
     return out
 
 def distance(a,b):
+    """
+    Euclidean distance between a and b.
+
+    :type a: NumPy array of shape (n,)
+    :type b: NumPy array of shape (n,)
+    :return: Distance between a and b
+    :rtype: float
+    """
     return np.sqrt(((a-b)**2).sum())

sandbox.py
@@ -17,8 +17,13 @@ np.set_printoptions(precision=3, threshold=np.inf, linewidth=14000, suppress=Tru
 #from molsym.symtext.multiplication_table import build_mult_table
 #print(build_mult_table(symels))
 
-import molsym
-from molsym.symtext.general_irrep_mats import pg_to_symels
+#import molsym
+#from molsym.symtext.general_irrep_mats import pg_to_symels
+#mol = molsym.Molecule.from_file("test/xyz/ammonia.xyz")
+#mol = molsym.symmetrize(mol)
+#s = molsym.Symtext.from_molecule(mol)
+#print(s.contains_symmetric_irrep(s.direct_product(0,1,1,2,2)))
+
 #from molsym.symtext.symtext_helper import get_class_name
 #symels, irreps, irrep_mats = pg_to_symels("Ih")
 
@@ -40,18 +45,18 @@ from molsym.symtext.general_irrep_mats import pg_to_symels
 #print(dp)
 #print(s.reduction_coefficients(dp))
 
-import molsym
-mol = molsym.Molecule.from_file("test/xyz/ammonia.xyz")
-mol = molsym.symmetrize(mol)
-s = molsym.Symtext.from_molecule(mol)
-fxn_list = ([[0,1],"R1"],[[0,2],"R2"],[[0,3],"R3"],[[1,0,2],"A1"],[[2,0,3],"A2"],[[3,0,1],"A3"])
-from molsym.salcs.internal_coordinates import InternalCoordinates
-from molsym.salcs.projection_op import ProjectionOp
-fxn_set = InternalCoordinates(s, fxn_list)
-salcs = molsym.salcs.ProjectionOp(s, fxn_set)
-print(salcs)
-print(salcs.sort_partner_functions())
-salcs.sort_to("blocks")
+#import molsym
+#mol = molsym.Molecule.from_file("test/xyz/ammonia.xyz")
+#mol = molsym.symmetrize(mol)
+#s = molsym.Symtext.from_molecule(mol)
+#fxn_list = ([[0,1],"R1"],[[0,2],"R2"],[[0,3],"R3"],[[1,0,2],"A1"],[[2,0,3],"A2"],[[3,0,1],"A3"])
+#from molsym.salcs.internal_coordinates import InternalCoordinates
+#from molsym.salcs.projection_op import ProjectionOp
+#fxn_set = InternalCoordinates(s, fxn_list)
+#salcs = molsym.salcs.ProjectionOp(s, fxn_set)
+#print(salcs)
+#print(salcs.sort_partner_functions())
+#salcs.sort_to("blocks")
 def doodoo(pg):
     symels, irreps, irrep_mat = pg_to_symels(pg)
     #for symel in symels:
@@ -62,6 +67,9 @@ def doodoo(pg):
     #print(irrep_mat)
     #print(irreps)
     #print(symels)
+    for symel in symels:
+        print(symel)
+        print(symel.vector)
     from molsym.symtext.goat import goat_chk, mtable_check
     from molsym.symtext.multiplication_table import build_mult_table
 
@@ -75,9 +83,10 @@ def doodoo(pg):
     print(mtab_chk, gchk)
 
 #aa = ["C1", "Ci", "Cs"] + [f"C{i}" for i in range(9)]
-#aa = [f"D{i}d" for i in range(2,9)]
+aa = [f"D{i}d" for i in [3,5]]
 #doodoo("Ih")
 #aa = ["C4v"]
-#for a in aa:
-#    print(a)
-#    doodoo(a)
+for a in aa:
+    print("-"*45)
+    print(a)
+    doodoo(a)

test/test_new_generator.py
@@ -30,18 +30,19 @@ def test_Symel(pg):
     rreps = [symel.symbol for symel in symels]
     rreps_ans = [symel.symbol for symel in symels_ans]
     assert set(rreps) == set(rreps_ans)
+    print(pg)
     for symel in symels:
         for symel_a in symels_ans:
             if symel.symbol == symel_a.symbol:
-                print(pg)
+                print(symel.symbol)
                 print(symel.vector)
                 print(symel_a.vector)
-                print(symel.rrep)
-                print(symel_a.rrep)
+                #print(symel.rrep)
+                #print(symel_a.rrep)
                 if symel.vector is None:
                     assert symel_a.vector is None
                 else:
-                    assert issame_axis(symel.vector, symel_a.vector).all()
+                    assert issame_axis(symel.vector, symel_a.vector)
                 assert np.isclose(symel.rrep, symel_a.rrep).all()
    
     mtable = build_mult_table(symels)