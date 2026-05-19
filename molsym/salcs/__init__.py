d5045b7cd64074aa9923979d60d174b56db0b302
Nate <nate.kitzmiller@indwes.edu>
2026-05-19 11:05:44 -0400

    cleanup pr

--- molsym/salcs/__init__.py
+++ molsym/salcs/__init__.py
@@ -2,8 +2,10 @@ from . import projection_op
 from . import cartesian_coordinates
 from . import internal_coordinates
 from . import spherical_harmonics
+from . import salc_tools
 
 from .projection_op import ProjectionOp
+from .projection_op import ProjectOnObject
 from .cartesian_coordinates import CartesianCoordinates
 from .internal_coordinates import InternalCoordinates
 from .spherical_harmonics import SphericalHarmonics
